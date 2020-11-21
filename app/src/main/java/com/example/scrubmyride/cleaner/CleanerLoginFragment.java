2c56563b7e7e54908eab0238c3b0a74bdb57ec5c
William Hepton <william.hepton@puzzel.com>
2020-11-21 15:07:23 +0000

    Database connection

--- app/src/main/java/com/example/scrubmyride/cleaner/CleanerLoginFragment.java
+++ app/src/main/java/com/example/scrubmyride/cleaner/CleanerLoginFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +13,13 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.example.scrubmyride.BackgroundWorker;
 import com.example.scrubmyride.R;
 
 public class CleanerLoginFragment extends Fragment {
+
+    EditText PhoneNumberET, PasswordET;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -22,6 +27,8 @@ public class CleanerLoginFragment extends Fragment {
     ) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.f_cleaner_login, container, false);
+        PhoneNumberET = view.findViewById((R.id.input_phone));
+        PasswordET = view.findViewById((R.id.input_password));
         return view;
     }
 
@@ -35,7 +42,14 @@ public class CleanerLoginFragment extends Fragment {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                navController.navigate(R.id.action_Cleaner_Login_to_Cleaner_Page);
+                String phone = PhoneNumberET.getText().toString();
+                String password = PasswordET.getText().toString();
+                String type = "login";
+
+                BackgroundWorker backgroundWorker = new BackgroundWorker(getActivity());
+                backgroundWorker.execute(type, phone, password);
+
+                //navController.navigate(R.id.action_Cleaner_Login_to_Cleaner_Page);
             }
         });
 
